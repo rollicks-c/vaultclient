1f7624b8d7a90c3f643c206c87b3a3ccf295f22c
Roli Christen <roland.christen@jaywalker.ch>
2024-04-01 14:49:47 +0200

    clean up

--- api.go
+++
@@ -1,9 +0,0 @@
-package vaultclient
-
-import (
-	"github.com/rollicks-c/vaultclient/vault"
-)
-
-func NewClient(addr string, options ...vault.Option) (*vault.Client, error) {
-	return vault.NewClient(addr, options...)
-}
